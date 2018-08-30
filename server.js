49dde0f1ae2fe6013e256d83bc21d3feee2a4537
Simone <simone.faggi@yahoo.it>
2018-08-30 09:53:13 +0200

    design fix

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use('/api', api); /* redirect to routes */
 app.use('/*', express.static(path.join(__dirname, 'Public')));
 
 app.listen(8000, function () {
-  console.log('Example app listening on port 8000!');
+  console.log('Trellopoly listening on port 8000!');
 });
 
 module.exports = app;
